591a0d94398c99b0c2f270c92fdada6e0b633de3
Artur Silvestre <artur.silvestreo@gmail.com>
2021-04-03 03:50:54 -0300

    Criado component NewTransaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,6 @@ export function Header({ onOpenNewTransactionModal }: HeaderProps) {
         <button type="button" onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
-        
       </Content>
     </Container>
   )
