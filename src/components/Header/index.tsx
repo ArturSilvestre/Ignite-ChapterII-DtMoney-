1d85d6511efbaf26d86f7b4c46ad47338ccb396a
Artur Silvestre <artur.silvestreo@gmail.com>
2021-04-03 02:56:44 -0300

    Inserido Comentario para funcao e interface criada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import logoImg from '../../assets/logo.svg'
 
 import { Container, Content } from './styles'
+//cria Interface para declarar Tipagem que a Prop esta recebendo.
 interface HeaderProps {
   onOpenNewTransactionModal: () => void;
 }
-
+// Passa Parametro para a funcao Header, onde a tipagem 'e igual a interface criado a cima.
 export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
